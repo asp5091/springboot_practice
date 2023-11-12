9d2cb3b7fa0246e3ee497cafa4fe86c2153ee7e2
asp5091 <asp5091@naver.com>
2023-11-13 01:29:48 +0900

    [Modify] Practice controller result transmission to Html inner text

--- src/main/kotlin/com/asp/sprb/controller/HtmlController.kt
+++ src/main/kotlin/com/asp/sprb/controller/HtmlController.kt
@@ -3,6 +3,7 @@ package com.asp.sprb.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class HtmlController {
@@ -12,8 +13,16 @@ class HtmlController {
         return "index"
     }
 
-    @GetMapping("/sign")
-    fun htmlForm(model: Model): String {
-        return "sign"
+    @GetMapping("/{formType}")
+    fun htmlForm(model: Model, @PathVariable formType: String): String {
+        model.addAttribute("title", formType.uppercase())
+        return "${formType}"
     }
+
+    /*
+    @GetMapping("/login")
+    fun htmlFormLogin(model: Model): String {
+        return "login"
+    }
+    */
 }
