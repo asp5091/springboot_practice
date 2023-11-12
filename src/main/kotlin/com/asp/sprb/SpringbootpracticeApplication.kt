f298406af12a02753571d9c4a67b21ac3598e748
asp5091 <asp5091@naver.com>
2023-11-12 16:42:04 +0900

    [ADD] First Commit spring boot init

---
+++ src/main/kotlin/com/asp/sprb/SpringbootpracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.asp.sprb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootpracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootpracticeApplication>(*args)
+}
